21b212f72b781dbefc73f9b9f9caa7c699ff08f4
lesquie <lucasefpro2@gmail.com>
2020-10-20 10:43:59 +0200

    Updated starting data set

--- schoolmanager-server/src/main/java/com/fr/schoolmanager/LoadDatabase.java
+++ schoolmanager-server/src/main/java/com/fr/schoolmanager/LoadDatabase.java
@@ -1,8 +1,8 @@
 package com.fr.schoolmanager;
 
+import com.fr.schoolmanager.evaluation.Evaluation;
 import com.fr.schoolmanager.skills.Skill;
 import com.fr.schoolmanager.skills.SkillRepository;
-import com.fr.schoolmanager.skills.SkillStatus;
 import com.fr.schoolmanager.students.Student;
 import com.fr.schoolmanager.students.StudentRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +15,6 @@ import org.springframework.context.annotation.Configuration;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
-import java.util.Date;
 
 @Configuration
 @Slf4j
@@ -31,17 +30,13 @@ public class LoadDatabase {
             Student s2 = new Student("Sam", "Sam",  LocalDate.of(2010, Month.NOVEMBER, 28),"sam@sam.lotro");
             Student s3 = new Student("Frodo", "Baggins",  LocalDate.of(2010, Month.MARCH, 1),"frodo@baggins.lotro");
 
-            Skill sk1 = new Skill("Skill 1", SkillStatus.NOT_ACQUIRED);
-            Skill sk2 = new Skill("Skill 2", SkillStatus.ACQUIRED);
-            Skill sk3 = new Skill("Skill 3", SkillStatus.IN_PROGRESS);
-            Skill sk4 = new Skill("Skill 4", SkillStatus.MASTERED);
-            Skill sk5 = new Skill("Skill 5", SkillStatus.NOT_ACQUIRED);
+            Skill sk1 = new Skill("Skill 1");
+            Skill sk2 = new Skill("Skill 2");
+            Skill sk3 = new Skill("Skill 3");
+            Skill sk4 = new Skill("Skill 4");
+            Skill sk5 = new Skill("Skill 5");
 
             skillRepository.saveAll(Arrays.asList(sk1, sk2, sk3, sk4, sk5));
-
-            s1.getSkills().addAll(Arrays.asList(sk1, sk2, sk3, sk4, sk5));
-            s2.getSkills().addAll(Arrays.asList(sk1, sk2, sk3, sk4, sk5));
-            s3.getSkills().addAll(Arrays.asList(sk1, sk2, sk3, sk4, sk5));
 
             logger.info("Preloading :" + studentRepository.save(s1));
             logger.info("Preloading :" + studentRepository.save(s2));
